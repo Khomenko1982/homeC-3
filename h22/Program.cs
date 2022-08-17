c8bc1a004bd7ff9a598337646337a515d606fbb2
«Andrey» <khomehko@mail.ru>
2022-08-17 05:39:09 +0300

    h22

---
+++ h22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+//N=Math.Abs(N);
+int nach=1;
+while (N>=nach)
+{
+  int sq=(nach*nach);
+Console.Write(sq);
+Console.Write(" ");
+nach++;
+}
