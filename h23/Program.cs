96db4f2e559139155060d8048bdcf7e85d6e3e03
«Andrey» <khomehko@mail.ru>
2022-08-17 05:48:32 +0300

    h23

---
+++ h23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+//N=Math.Abs(N);
+int nach=1;
+while (N>=nach)
+{
+  int sq=(nach*nach*nach);
+Console.Write(sq);
+Console.Write(" ");
+nach++;
+}
